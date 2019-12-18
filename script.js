16f6f3bd514ed7d843ce653aa483d2388c4a2449
Alissa <alissabc@hotmail.com>
2019-12-17 23:23:06 -0500

    new style changes

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 $(function(){
+    //Hide and fade in heading in header on load
     $(".header__text").hide()
     $(".arrow").hide()
     setTimeout(function(){
         $(".header__text").fadeIn("900");
     }, 500)
-
+//Set timeout fade in arrow button in header
     setTimeout(function(){
         $(".arrow").fadeIn("900");
     }, 1500)
-
+//menu button toggle display
     $("#navButton").on("click", function(){
         $(".main-nav").toggleClass("display")
         $(".display").hide();
         $(".display").fadeIn("900")
     })
-
+    //Header arrow scroll button to about me section
     $(".scroll").on("click", function (){
         $("body,html").animate(
             {
@@ -24,7 +25,7 @@ $(function(){
         );
     })
 
-    //slide in
+    //slide in animate on scroll
     AOS.init();
     AOS.init({
         disable: "mobile"
